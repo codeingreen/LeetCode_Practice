869c4d27baf1184dc474ddffba3e9bfaef645591
mmkhaque <50067081+mmkhaque@users.noreply.github.com>
2021-05-26 08:44:33 -0700

    https://leetcode.com/problems/combination-sum/

---
+++ 39. Combination Sum.py
@@ -0,0 +1,64 @@
+'''
+Given an array of distinct integers candidates and a target integer target, 
+return a list of all unique combinations of candidates where the chosen numbers sum to target. 
+You may return the combinations in any order.
+
+The same number may be chosen from candidates an unlimited number of times. 
+Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+
+It is guaranteed that the number of unique combinations that sum up 
+to target is less than 150 combinations for the given input.
+
+ 
+
+Example 1:
+Input: candidates = [2,3,6,7], target = 7
+Output: [[2,2,3],[7]]
+Explanation:
+2 and 3 are candidates, and 2 + 2 + 3 = 7. Note that 2 can be used multiple times.
+7 is a candidate, and 7 = 7.
+These are the only two combinations.
+
+Example 2:
+Input: candidates = [2,3,5], target = 8
+Output: [[2,2,2,2],[2,3,3],[3,5]]
+
+Example 3:
+Input: candidates = [2], target = 1
+Output: []
+https://leetcode.com/problems/combination-sum/solution/
+'''
+
+from itertools import permutations, combinations
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        
+        result = []
+        
+        def backtrack(remain, comb, start):
+            
+            # base condition
+            if remain == 0:
+                result.append(list(comb))
+                return result
+            elif remain < 0:
+                # outside the limit, exit
+                return
+            
+            for i in range(start, len(candidates)):
+                # add candidate in the combination
+                comb.append(candidates[i])
+                
+                backtrack(remain- candidates[i], comb, i)
+                
+                # after backtrack remove the number from the combination
+                comb.pop()
+                
+                
+        backtrack(target, [], 0)
+        
+        return result
+                    
+        
